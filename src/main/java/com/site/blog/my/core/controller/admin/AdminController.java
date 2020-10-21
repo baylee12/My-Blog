a55b92e5271305ee0b8d777821740e58e23dbc61
baylee <jstybaylee@it888.com>
2020-10-21 20:42:56 +0800

    6

--- src/main/java/com/site/blog/my/core/controller/admin/AdminController.java
+++ src/main/java/com/site/blog/my/core/controller/admin/AdminController.java
@@ -82,7 +82,7 @@ public class AdminController {
         } else {
             session.setAttribute("errorMsg", "登陆失败");
             return "admin/login";
-
+        // 1
             
 
         }
